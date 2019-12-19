58b4904c42c04234b8f5528ab74be49b7171fd50
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2019-12-19 20:36:46 +0300

    Sayilar Component

---
+++ ViewComponentGiris/ViewComponents/SayilarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponentGiris.ViewComponents
+{
+    public class SayilarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int minNumber, int maxNumber)
+        {
+            List<int> sayilar = Enumerable.Range(minNumber, maxNumber - minNumber + 1).ToList();
+            
+            return View(sayilar);
+        }
+    }
+}
